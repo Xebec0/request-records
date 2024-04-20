5e5a2d4da8bf09a69bbbe33060b485f6469599cf
Lexra23 <74659632+Lexra23@users.noreply.github.com>
2024-04-20 19:34:53 +0800

    Added login and register index page

--- records/records/urls.py
+++ records/records/urls.py
@@ -1,13 +1,19 @@
-
 from django.contrib import admin
 from django.urls import path
 from onlinerequest.views import views
-
+from onlinerequest.views import register
+from onlinerequest.views import login
 
 # Define URL paths here
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Main index page
-    path('', views.index)
+    path('', views.index),
+
+    # Register
+    path('register/', register.index),
+
+    # Login
+    path('login/', login.index)
 ]
