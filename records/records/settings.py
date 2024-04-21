77eaf45ade91671738ff9bcdcf2112128a366c9d
wRayleigh <115050541+wRayleigh@users.noreply.github.com>
2024-04-21 12:16:42 +0800

    Added Django Bootstrap 5

--- records/records/settings.py
+++ records/records/settings.py
@@ -27,7 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'onlinerequest.apps.OnlinerequestConfig'
+    'onlinerequest.apps.OnlinerequestConfig',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
