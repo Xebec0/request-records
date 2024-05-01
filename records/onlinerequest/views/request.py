ff5fd1fb59a179b935d8dc2d867aeb185e779eee
Lexra23 <74659632+Lexra23@users.noreply.github.com>
2024-05-01 18:49:21 +0800

    Admin approval of request

--- records/onlinerequest/views/request.py
+++ records/onlinerequest/views/request.py
@@ -6,7 +6,8 @@ from ..models import Request
 from ..models import Requirement
 from ..models import User_Request
 from ..serializers import RequestSerializer
-from django.core import serializers
+from django.conf import settings
+import os
 
 
 def index(request):
@@ -32,28 +33,47 @@ def display_user_requests(request):
     return render(request, 'admin/request/user-request.html', {'user_requests': user_requests })
 
 def display_user_request(request, id):
-    user_request = User_Request.objects.get(id = id)
+    if request.method == "POST":
 
-    uploads = []
-    for record in user_request.uploads.split(','):
-        if record:
-            test = record.replace(">", "").replace("<", "").split('&')
-            print(test[0] + "-" + test[1])
+        requested_file = request.FILES.get('requested_file')
 
-    for user_request_upload in user_request.uploads.split(','):
-        if user_request_upload:
-            upload = user_request_upload.replace("<", "").replace(">", "").split('&')
-            uploads.append({
-                'code' : getCodeDescription(Requirement, upload[0]),
-                'path' : upload[1]
-            })
+        if requested_file is None:
+            return JsonResponse({'status': False, 'message' : "Please submit the requested file before approving the request."})
 
-    context = {
-        'user_request': user_request,
-        'uploads': uploads,
-    }        
+        file_path = handle_uploaded_file(id, requested_file)
+
+        # Update user_request
+        user_request = User_Request.objects.get(id = id)
+        user_request.requested = file_path
+        user_request.status = "Completed"
+        user_request.save()
+
+        return JsonResponse({'status': True, 'message': 'Request completed. Please head back to the request tab.', 'request_status' : user_request.status})
+    else:
+        user_request = User_Request.objects.get(id = id)
+
+        uploads = []
+        for record in user_request.uploads.split(','):
+            if record:
+                test = record.replace(">", "").replace("<", "").split('&')
+                print(test[0] + "-" + test[1])
+
+        for user_request_upload in user_request.uploads.split(','):
+            if user_request_upload:
+                upload = user_request_upload.replace("<", "").replace(">", "").split('&')
+                uploads.append({
+                    'code' : getCodeDescription(Requirement, upload[0]),
+                    'path' : upload[1]
+                })
+
+        context = {
+            'user_request': user_request,
+            'uploads': uploads,
+        }      
 
     return render(request, 'admin/request/view-user-request.html', context)
+
+
 
 def getCodeDescription(model, key):
     model_instance = model.objects.get(code = key)
@@ -79,3 +99,19 @@ def get_requests(request):
     requests = Request.objects.all()
     requests_json = RequestSerializer(requests, many=True).data  # Serialize the queryset
     return JsonResponse(requests_json, safe=False)
+
+def handle_uploaded_file(source, file):
+    # Define the path where you want to save the file
+    static_dir = os.path.join(settings.MEDIA_ROOT, 'onlinerequest', 'static', 'user_request', str(source), 'approved')
+
+    # Create the upload directory if it doesn't exist
+    if not os.path.exists(static_dir):
+        os.makedirs(static_dir)
+
+    # Save the file
+    file_path = os.path.join(static_dir, file.name)
+    with open(file_path, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+
+    return file_path
