9c00557533f0af0f92142b8b7cebc21e1a0a6038
wRayleigh <115050541+wRayleigh@users.noreply.github.com>
2024-05-20 01:16:25 +0800

    Debugging

--- records/onlinerequest/views/register.py
+++ records/onlinerequest/views/register.py
@@ -12,30 +12,17 @@ from ..models import Profile
 import os
 
 def index(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
-    
         if form.is_valid():
-            # User is saved
             user = form.save()
-
-            # Profile is saved
-            create_profile(user)
-
-            # Access the uploaded file
-            uploaded_file = request.FILES.get('formFile')
-
-            if uploaded_file:
-                # Save the file to a specific location
-                file_path = handle_uploaded_file(uploaded_file)
-                RegisterRequest.objects.create(user = user, valid_id = file_path)
-            
-            return JsonResponse({'status' : True, 'message' : "Registered succesfully"})
+            return JsonResponse({'status': True, 'message': 'User registered successfully'})
         else:
-            last_error_message = list(form.errors.items())[0]
-            return JsonResponse({'status' : False,'message' : last_error_message[1]})
+            errors = form.errors.as_json()
+            return JsonResponse({'status': False, 'message': 'Validation errors', 'errors': json.loads(errors)})
     else:
-        return render(request, "register.html")
+        form = UserRegistrationForm()
+    return render(request, 'register.html', {'form': form})
     
 def handle_uploaded_file(file):
     # Define the path where you want to save the file
