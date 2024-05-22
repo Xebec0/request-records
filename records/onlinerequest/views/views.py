73afc358cbfabf9d6c2d46609f80c7597cb569af
Susedge <166993665+Susedge@users.noreply.github.com>
2024-05-22 15:15:12 +0800

    Landing page - Guest UI enhancement

--- records/onlinerequest/views/views.py
+++ records/onlinerequest/views/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import logout
 from django.shortcuts import redirect
+from onlinerequest.models import Request, Requirement
 
-# Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    request_forms = Request.objects.all()
+    for request_form in request_forms:
+        requirement_descriptions = []
+        for requirement_code in request_form.files_required.split(','):
+            try:
+                requirement = Requirement.objects.get(code=requirement_code)
+                requirement_descriptions.append(requirement.description)
+            except Requirement.DoesNotExist:
+                requirement_descriptions.append(requirement_code)
+        request_form.requirement_descriptions = requirement_descriptions
+    return render(request, 'index.html', {'request_forms': request_forms})
 
 def signup_view(request):
     return render(request, 'signup.html')
